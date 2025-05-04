14915c1736ced63b31858129989fdc054bc3d7de
Jayden Melendez <jayden.melendez1@marist.edu>
2025-05-04 21:25:17 +0200

    Icons and labels of icons added to tab bar

--- SplitIt/app/tabs/_layout.tsx
+++ SplitIt/app/tabs/_layout.tsx
@@ -1,11 +1,42 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons'; // Or any other icon pack
 
+// Tab bar layout
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-      <Tabs.Screen name="index" options={{ title: 'Account' }} />
-      <Tabs.Screen name="capture" options={{ title: 'Capture' }} />
-      <Tabs.Screen name="history" options={{ title: '', }}/>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'blue',
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="capture"
+        options={{
+          title: 'Scan',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="camera-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: 'History',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="time-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
