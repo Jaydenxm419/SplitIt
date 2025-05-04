04c435b4de1a90074512eea72eec0184dbb406b9
Jayden Melendez <jayden.melendez1@marist.edu>
2025-05-04 21:52:15 +0200

    Scan page created

--- SplitIt/app/tabs/capture.tsx
+++ SplitIt/app/tabs/capture.tsx
@@ -1,11 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function CaptureScreen() {
+export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Button onPress={requestPermission} title="Scan new receipt" />
+      </View>
+    );
+  }
+
+  function takePicture() {
+    console.log("OH SNAP")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Details</Text>
-      <Link href="/tabs/history">Access History</Link>
+      <CameraView style={styles.camera} facing={'back'}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Capture</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
@@ -14,6 +37,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
     alignItems: 'center',
   },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5
+  },
 });
+
